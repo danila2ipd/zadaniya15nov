abc50abfcf2066c41a54912a683ac0ebb3fc88db
SeRt4 <perevezserg@gmail.com>
2023-11-15 15:21:12 +0300

    litdawayy

--- zadanie2.15.cpp
+++ zadanie2.15.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "russian");
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
 	int sum = 0;
 	for (int i = 0; i < 10; i++)
 	{
